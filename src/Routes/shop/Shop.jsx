863cead216a961ba5fb082c6e4b9b9a28de9b6ca
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-02-27 19:16:22 -0800

    created product card

--- src/Routes/shop/Shop.jsx
+++ src/Routes/shop/Shop.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useContext } from 'react'
-
 import { ProductsContext } from '../../context/ProductsContext'
+import ProductCard from '../../components/ProductCard/ProductCard'
 
 function Shop() {
-  const { products } = useContext
+  const { products } = useContext(ProductsContext)
   console.log('products', products)
   return (
     <div>
-      {products.map(({name, id}) => (
-        <div key={id}><h1>{name}</h1></div>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
     ))}
     </div>
   )
