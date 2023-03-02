d055cb1ba4cc358ba3ca6b7b0b7f7eddc5d50a0b
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-02 08:06:34 -0800

    shop section only shows a previw of 4 items

--- src/Routes/shop/Shop.jsx
+++ src/Routes/shop/Shop.jsx
@@ -2,25 +2,21 @@ import React from 'react'
 import { Fragment } from 'react'
 import { useContext } from 'react'
 import { CategoriesContext } from '../../context/CategoriesContext'
-import ProductCard from '../../components/ProductCard/ProductCard'
+import CategoryPreview from '../../components/categoryPreview/CategoryPreview'
 import './shop.styles.scss'
 
 function Shop() {
   const { categoriesMap } = useContext(CategoriesContext)
   console.log('products', categoriesMap)
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-      </div>
-    </Fragment>
-        ))}
-    </Fragment>
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+
+        return <CategoryPreview key={title} title={title} products={products} />
+
+      })}
+    </div>
   )
 }
 
