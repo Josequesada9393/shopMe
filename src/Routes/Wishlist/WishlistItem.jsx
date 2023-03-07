37810e531d8d3d1482f005b11afc1015ff051e4b
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-07 11:58:52 -0800

    fixed css wishlist

--- src/Routes/Wishlist/WishlistItem.jsx
+++ src/Routes/Wishlist/WishlistItem.jsx
@@ -2,10 +2,15 @@ import React, { useContext } from 'react'
 import './WishListItemStyles.scss'
 import { BsHeartbreakFill } from 'react-icons/bs'
 import { WishListContext } from '../../context/WishListContext';
+import { CartContext } from '../../context/CartContext';
+import { InvertedButton } from '../../components/button/Button.styles';
 
 function WishlistItem({ item }) {
 
-  const {removeItemFromWishList} = useContext(WishListContext)
+  const { removeItemFromWishList } = useContext(WishListContext)
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(item);
 
   const removeItem = () => {
     console.log(item)
@@ -13,15 +18,14 @@ function WishlistItem({ item }) {
   }
   const { imageUrl, name, price } = item;
   return (
-    <div>
       <div className='WishList-card-container'>
         <BsHeartbreakFill onClick={removeItem} className='removeIcon'/>
-      <img src={imageUrl} alt={`${name}`} />
+      <img className="imageWishList" src={imageUrl} alt={`${name}`} />
       <div className='footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-    </div>
+     <InvertedButton className="addToCartButton" buttonType='inverted' onClick={addProductToCart}>Add to Cart</InvertedButton>
     </div>
   )
 }
