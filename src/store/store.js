6f1f1286c8b6ef193abd3d75389ce2653155edfe
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-15 10:14:10 -0700

    persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,28 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger'
+import {persistStore, persistReducer} from 'redux-persist'
+import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistConfig = {
+  key: 'root',
+  storage,
+  //we do not want our user to persist
+  blacklist: ['user']
+}
 
-export const Store = createStore(rootReducer, undefined, composedEnhancers)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+//use middleware only if  in production
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+// user redux tools only if we are in production
+const composeEnhancer = (process.env.ENV_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store);
